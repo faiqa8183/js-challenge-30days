2ab8131d336c91b5b7a0e01dc5116ef95c1864e3
Faiqa Malik <105700456+faiqa8183@users.noreply.github.com>
2024-07-14 23:59:44 +0500

    Add files via upload

---
+++ challenge-01.js
@@ -0,0 +1,25 @@
+///different declarations
+let varOne = 2;
+console.log(" let " ,varOne);
+
+var varTwo = "faiqa"
+console.log(" Var " ,varTwo);
+
+const constOne = 2
+console.log(" Const " ,constOne);
+
+//typeof 
+console.log(typeof varOne);
+console.log(typeof varTwo);
+console.log(typeof constOne);
+
+//Reassignment check
+varTwo = 3
+console.log(varTwo);
+
+//constOne = 5
+//console.log(constOne); const val cant be changed 
+
+
+
+
